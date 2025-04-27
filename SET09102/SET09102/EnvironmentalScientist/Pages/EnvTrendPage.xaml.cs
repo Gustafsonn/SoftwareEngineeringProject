aeb0990cb70ecad54aee6378ed2bdd5cf68ff930
MagnusMacphee <123734481+Gustafsonn@users.noreply.github.com>
2025-04-27 19:37:49 +0100

    UI for trend report created

--- SET09102/SET09102/EnvironmentalScientist/Pages/EnvTrendPage.xaml.cs
+++ SET09102/SET09102/EnvironmentalScientist/Pages/EnvTrendPage.xaml.cs
@@ -1,3 +1,7 @@
+using System;
+using System.Collections.Generic;
+using Microsoft.Maui.Controls;
+
 namespace SET09102.EnvironmentalScientist.Pages;
 
 public partial class EnvTrendPage : ContentPage
@@ -5,5 +9,31 @@ public partial class EnvTrendPage : ContentPage
 	public EnvTrendPage()
 	{
 		InitializeComponent();
+		LoadMockData();
+	}
+
+	private void LoadMockData()
+	{
+		// Mock parameters
+		var parameters = new List<string> { "Temperature", "Humidity", "CO2" };
+		ParameterPicker.ItemsSource = parameters;
+
+		// Mock locations
+		var locations = new List<string> { "Sensor 1", "Sensor 2" };
+		LocationPicker.ItemsSource = locations;
+
+		// Set default dates
+		StartDatePicker.Date = DateTime.Now.AddMonths(-1);
+		EndDatePicker.Date = DateTime.Now;
+	}
+
+	private async void OnGenerateReportClicked(object sender, EventArgs e)
+	{
+		if (ParameterPicker.SelectedItem == null || LocationPicker.SelectedItem == null)
+		{
+			await DisplayAlert("Error", "Please select both a parameter and location", "OK");
+			return;
+		}
+		await DisplayAlert("Report", $"Generating report for {ParameterPicker.SelectedItem} at {LocationPicker.SelectedItem} from {StartDatePicker.Date:d} to {EndDatePicker.Date:d}", "OK");
 	}
 }
