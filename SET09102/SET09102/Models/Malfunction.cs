7c98a183d54f656b95c72d8095796d59e8f4fc87
Andrew Latto <158503199+andrewlatto@users.noreply.github.com>
2025-05-03 17:21:09 +0100

    Add support for saving malfunctions to the database; implement add and resolve functionality for sensor malfunction records. (#30)

---
+++ SET09102/SET09102/Models/Malfunction.cs
@@ -0,0 +1,89 @@
+﻿using System.ComponentModel;
+using System.Runtime.CompilerServices;
+
+namespace SET09102.Models;
+
+public class Malfunction : INotifyPropertyChanged
+{
+    private int _id;
+    private int _sensorId;
+    private string _description = string.Empty;
+    private bool _resolved;
+
+    public int Id
+    {
+        get => _id;
+        set
+        {
+            if (_id != value)
+            {
+                _id = value;
+                OnPropertyChanged();
+            }
+        }
+    }
+
+    public int SensorId
+    {
+        get => _sensorId;
+        set
+        {
+            if (_sensorId != value)
+            {
+                _sensorId = value;
+                OnPropertyChanged();
+            }
+        }
+    }
+
+    public string Description
+    {
+        get => _description;
+        set
+        {
+            if (_description != value)
+            {
+                _description = value;
+                OnPropertyChanged();
+            }
+        }
+    }
+
+    public bool Resolved
+    {
+        get => _resolved;
+        set
+        {
+            if (_resolved != value)
+            {
+                _resolved = value;
+                OnPropertyChanged();
+                OnPropertyChanged(nameof(Active));
+            }
+        }
+    }
+
+    public Malfunction Clone()
+    {
+        return new()
+        {
+            Id = Id,
+            SensorId = SensorId,
+            Description = Description,
+            Resolved = Resolved
+        };
+    }
+
+    public bool Active
+    {
+        get => !Resolved;
+        set => Resolved = !value;       
+    }
+
+    public event PropertyChangedEventHandler? PropertyChanged;
+
+    protected virtual void OnPropertyChanged([CallerMemberName] string? propertyName = null)
+    {
+        PropertyChanged?.Invoke(this, new PropertyChangedEventArgs(propertyName));
+    }
+}
